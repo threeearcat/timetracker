74c49d7535fb4a8c059860c4a12c1f78b42f7191
Dae R. Jeong <threeearcat@gmail.com>
2022-08-22 17:47:15 +0900

    implement pomodoro timer

--- timetracker.py
+++ timetracker.py
@@ -11,6 +11,12 @@ import ctypes
 import ctypes.util
 import json
 
+import gi
+gi.require_version('Notify', '0.7')
+from gi.repository import Notify
+Notify.init('Timetracker')
+
+
 class IdleTracker(object):
     def __init__(self):
 
@@ -244,17 +250,80 @@ class FocusTracker(object):
 
 
 class PomodoroTimer(object):
+    idle = 0
+    resting = 1
+    # Working should be the last as I encode the round into self.state
+    working = 2
+
     def __init__(self):
-        pass
+        self.round_per_session = 2
+        self.rest_time_in_session = 5
+        self.rest_time_after_session = 20
+        self.working_time = 30
+        self._reset()
+
+
+    def _reset(self):
+        self.state = PomodoroTimer.idle
+
+
+    def round(self):
+        return self.state - PomodoroTimer.working if self.state >= PomodoroTimer.working else self.round_per_session
+
+
+    def _notify(self, title, msg):
+        notify = Notify.Notification.new(title, msg)
+        notify.show()
+
+
+    def arm_timer(self, time_mins, callback):
+        self.timer = threading.Timer(time_mins * 60, callback)
+        self.timer.start()
+
+
+    def start_resting(self):
+        current_round = self.round()
+        self.state += 1
+        next_round = current_round + 1
+
+        if next_round % self.round_per_session == 0:
+            rest_time = self.rest_time_after_session
+            message = "Session done."
+        else:
+            rest_time = self.rest_time_in_session
+            message = "Round {} done.".format(current_round)
+        message += " Resting for {} mins.".format(rest_time)
+
+        self._notify("Start resting", message)
+        self.arm_timer(rest_time, self.start_round)
+
+
+    def start_round(self):
+        round = self.round()
+        self._notify("Start working", "Round {} for {} mins".format(round, self.working_time))
+        self.arm_timer(self.working_time, self.start_resting)
+
 
     def run(self):
-        pass
+        if not self.state == PomodoroTimer.idle:
+            return
+        self.state = PomodoroTimer.working
+        self.start_round()
+
 
     def stop(self):
-        pass
+        if self.timer != None and self.timer.is_alive():
+            self.timer.cancel()
+            self.timer = None
+        self._reset()
+
 
     def reset(self):
-        pass
+        was_running = self.state >= PomodoroTimer.working
+        self.stop()
+        self._reset()
+        if was_running:
+            self.run()
 
 
 class WorkingHourManager(object):
